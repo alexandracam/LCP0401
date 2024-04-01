a27f72e11608dcf92abf0616c1dbf80f17b2b138
Alexandra Camarena <alexandracamarena@ou.edu>
2024-04-01 00:33:11 -0500

    Add project files.

---
+++ LCP0401/GetAddress (1).cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+namespace mis3013
+{
+    public static class GetAddress
+    {
+        //public static IntPtr Get(Object o)
+        //{
+        //    GCHandle gch = GCHandle.Alloc(o, GCHandleType.Pinned);
+        //    IntPtr pObj = gch.AddrOfPinnedObject();
+        //    return pObj;
+        //}
+
+        public static long Get(Object o)
+        {
+            GCHandle gch = GCHandle.Alloc(o, GCHandleType.Pinned);
+            IntPtr pObj = gch.AddrOfPinnedObject();
+            return (long)pObj;
+        }
+
+        //public static string Get(Object o)
+        //{
+        //    GCHandle gch = GCHandle.Alloc(o, GCHandleType.Pinned);
+        //    IntPtr pObj = gch.AddrOfPinnedObject();
+        //    return $"The address is: {(long)pObj}";
+        //}
+
+
+
+    }
+
+}
---
+++ LCP0401/Program.cs
@@ -0,0 +1,13 @@
+﻿// MIS 3013 001
+// April 1, 2024
+// Alexandra Camarena
+// 113568154
+
+// strings
+string str1;
+string str2;
+
+str2 = new string("MIS 3013");
+
+Console.WriteLine(str2.Length);
+Console.WriteLine(str2);
